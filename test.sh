08c84a736f6b97865d99682f14323ebaa137c46f
Sulverus <sulverus@gmail.com>
2015-12-04 18:10:24 +0300

    Mysql settings moved to travis

--- test.sh
+++ test.sh
@@ -8,11 +8,6 @@ sudo apt-get update > /dev/null
 sudo apt-get -q -y install tarantool tarantool-dev
 sudo apt-get -q -y install libmysqlclient-dev
 
-mysql -e "CREATE USER 'tarantool'@'localhost' IDENTIFIED BY 'tarantool';" -u root
-mysql -e "CREATE DATABASE tarantool;" -u root
-mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'tarantool'@'localhost' WITH GRANT OPTION;" -u root
-export MYSQL='127.0.0.1:3306:tarantool:tarantool:tarantool'
-
 cmake . -DCMAKE_BUILD_TYPE=RelWithDebugInfo
 make
 make test
