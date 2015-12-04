22ec026e19a268daa3f715281b74a77107fbd7c1
Sulverus <sulverus@gmail.com>
2015-12-04 17:27:40 +0300

    Fix test.sh apt

--- test.sh
+++ test.sh
@@ -5,8 +5,8 @@ lsb_release -c -s
 curl http://tarantool.org/dist/public.key | sudo apt-key add -
 echo "deb http://tarantool.org/dist/master/ubuntu/ `lsb_release -c -s` main" | sudo tee -a /etc/apt/sources.list.d/tarantool.list
 sudo apt-get update > /dev/null
-sudo apt-get -q install tarantool tarantool-dev
-sudo apt-get -q install libmysqlclient-dev
+sudo apt-get -q -y install tarantool tarantool-dev
+sudo apt-get -q -y install libmysqlclient-dev
 
 mysql -e "CREATE USER 'tarantool'@'localhost' IDENTIFIED BY 'tarantool';" -u root
 mysql -e "CREATE DATABASE tarantool;" -u root
